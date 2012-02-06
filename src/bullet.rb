51c41a2364eec3efab261cb1e8c12bbabc3a21ed
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-06 15:54:27 +0100

    [REFACTOR] attr_reader used
    In order to simplify Bullet attr_reader is used.

--- src/bullet.rb
+++ src/bullet.rb
@@ -1,14 +1,7 @@
 class Bullet
+  attr_reader :location, :boundingBox
   def initialize(location = Location.new(0,0), boundingBox = BoundingBox.new(0,0))
     @location = location
     @boundingBox = boundingBox
-  end
-
-  def location
-    return @location
-  end
-
-  def boundingBox
-    return @boundingBox
   end
 end
