b5975c380e1cfa0686ef619839cd96a320d8d071
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-07 12:11:56 +0100

    [REFACTOR] different wording
    Differents words are used to describe the collision-detector spec.
    The intention of the specification is clearer.

--- spec/collision_spec.rb
+++ spec/collision_spec.rb
@@ -1,6 +1,6 @@
 require 'collision-detector'
 
-describe "being hit" do
+describe "collision-detector" do
   before(:each) do
     @detector = CollisionDetector.new
   end
@@ -9,21 +9,21 @@ describe "being hit" do
     @alien = Alien.new(Location.new(0,0), BoundingBox.new(4,4))
   end
   
-  it "should hit an alien dead on" do
+  it "should detect an dead on collision" do
     bullet = Bullet.new(Location.new(0,0))
     
     @detector.collides?(@alien,bullet).should == true
   end
 
-  it "should mis an alien if it is to the right of its bounding box" do
+  it "should not detect a collision when way off." do
     bullet = Bullet.new(Location.new(6,0))
 
-    @detector.collides?(alien, bullet).should == false
+    @detector.collides?(@alien, bullet).should == false
   end
 
-  it "should mis an alien if it is below of its bounding box" do
-    bullet = Bullet.new(Location.new(0,-6))
+  it "should detect a glancing blow" do
+    bullet = Bullet.new(Location.new(0,-4))
 
-    @detector.collides?(@alien, bullet).should == false
+    @detector.collides?(@alien, bullet).should == true
   end
 end
