a6fcec75aac797af2f8dded15fd5c17108e6f34e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-20 19:32:40 +0100

    Alien shown
    An alien is shown on the screen.

--- client.rb
+++ client.rb
@@ -5,12 +5,26 @@ require 'bundler/setup'
 
 require 'rubygame'
 
+class Alien
+  def initialize()
+    @surface = Rubygame::Surface.load("resource/image/alien.png")
+    @x = 10
+    @y = 10
+  end
+  
+  def blit(screen)
+    @surface.blit(screen,[@x, @y])
+  end
+end
+
+
 class GuiView
   include Rubygame
 
   def initialize()
     @screen = Screen.new [640, 480]
     @events = EventQueue.new
+    @aliens = [Alien.new]
   end
 
   def eventLoop()
@@ -21,7 +35,16 @@ class GuiView
           return
         end
       end
+      draw
     end
+  end
+
+  def draw()
+    @screen.fill [0, 0, 0]
+    @aliens.each do |alien|
+      alien.blit(@screen)
+    end
+    @screen.update
   end
 end
 
