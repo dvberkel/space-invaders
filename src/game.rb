54f6489c2df49a5f04040df7eb43abfc872e1025
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-02-26 15:46:51 +0100

    ALien movement
    The aliens zigzag down.

--- src/game.rb
+++ src/game.rb
@@ -4,10 +4,11 @@ require 'location'
 
 class Game < SpaceInvaders
   def start
+    @strategy = ZigZag.new(Vector.new(3,0), -400, 400, 5)
     (-4..4).each do |ix|
       (0..2).each do |iy|
         x,y = 80 * ix, 500 - 50 * iy
-        addAlien(Alien.new({:location => Vector.new(x,y)}))
+        addAlien(Alien.new({:location => Vector.new(x,y), :moveStrategy => @strategy}))
       end
     end
     @gun = Gun.new({:moveStrategy => EventedMoveStrategy.new})
@@ -15,11 +16,45 @@ class Game < SpaceInvaders
   end
 
   def tick
+    @strategy.determineDirection
     move
     collisionDetection
   end
 
   def signal(signal)
     @gun.notify(signal)
+  end
+end
+
+class ZigZag < MovementStrategy
+  def initialize(displacement, minimum, maximum, drop)
+    @displacement = displacement
+    @minimum = minimum
+    @maximum = maximum
+    @drop = Vector.new(0,-drop)
+    @direction = 1
+    @shouldSwitch = false
+    @shouldDrop = false
+  end
+
+  def move(location)
+    if (@direction > 0 and location.x > @maximum) or (@direction < 0 and location.x < @minimum)
+      @shouldSwitch = true
+    end
+    if @shouldDrop
+      location = location.displaceBy(@drop)
+    end
+    location.displaceBy(@displacement.times(@direction))   
+  end
+
+  def determineDirection 
+    if @shouldDrop
+      @shouldDrop = false
+    end
+    if @shouldSwitch
+      @direction *= -1
+      @shouldSwitch = false
+      @shouldDrop = true
+    end
   end
 end
